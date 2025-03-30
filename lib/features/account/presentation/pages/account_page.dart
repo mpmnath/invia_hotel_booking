fae0e20cf6cfa9ebf5fd20aa0169169b8e1b25c6
mpmnath <mpm.nath007@gmail.com>
2025-03-30 01:48:20 +0100

    Add initial page implementations for account, favorites, hotels, and overview features

---
+++ lib/features/account/presentation/pages/account_page.dart
@@ -0,0 +1,12 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class AccountPage extends StatelessWidget {
+  const AccountPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Welcome to the Account Page'));
+  }
+}
---
+++ lib/features/hotels/presentation/pages/hotels_page.dart
@@ -0,0 +1,12 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class HotelsPage extends StatelessWidget {
+  const HotelsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Welcome to the Hotels Page'));
+  }
+}
