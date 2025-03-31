d0760a76ba6469e4308b7d0fce374aab824ba935
mpmnath <mpm.nath007@gmail.com>
2025-03-31 12:20:16 +0200

    Add failure handling classes for server, network, and cache errors

---
+++ lib/core/failure/failure.dart
@@ -0,0 +1,20 @@
+abstract class Failure {}
+
+class ServerFailure extends Failure {
+  final String message;
+  final int? code;
+
+  ServerFailure(this.message, {this.code});
+}
+
+class NetworkFailure extends Failure {
+  final String message;
+
+  NetworkFailure(this.message);
+}
+
+class CacheFailure extends Failure {
+  final String message;
+
+  CacheFailure(this.message);
+}
