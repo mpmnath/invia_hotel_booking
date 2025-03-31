78f09f2149addd688f8eb9317844e861e111f461
mpmnath <mpm.nath007@gmail.com>
2025-03-31 23:15:33 +0200

    Add HotelsCubit and state management for hotel listings

--- lib/features/hotels/presentation/cubits/hotels_cubit.dart
+++ lib/features/hotels/presentation/cubits/hotels_cubit.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
 import 'package:invia_hotel_booking/core/domain/entities/hotel.dart';
 import 'package:invia_hotel_booking/core/usecases/usecases.dart';
+import 'package:invia_hotel_booking/features/favorites/domain/usecases/stream_favorites_usecase.dart';
 import 'package:invia_hotel_booking/features/hotels/domain/usecases/get_hotels_usecase.dart';
 
 part 'hotels_state.dart';
@@ -10,15 +11,18 @@ part 'hotels_state.dart';
 @lazySingleton
 class HotelsCubit extends Cubit<HotelsState> {
   final GetHotelsUseCase _getHotelsUseCase;
-  HotelsCubit({required GetHotelsUseCase getHotelsUseCase})
-    : _getHotelsUseCase = getHotelsUseCase,
-      super(HotelsInitial());
+
+  HotelsCubit({
+    required GetHotelsUseCase getHotelsUseCase,
+    required StreamFavoritesUsecase streamFavoritesUsecase,
+  }) : _getHotelsUseCase = getHotelsUseCase,
+       super(HotelsInitial());
 
   void getHotels() async {
     emit(HotelsLoading());
-    final result = await _getHotelsUseCase(NoParams());
+    final hotels = await _getHotelsUseCase(NoParams());
     emit(
-      result.fold(
+      hotels.fold(
         (error) => HotelsError(error.toString()),
         (hotels) => HotelsLoaded(hotels),
       ),
