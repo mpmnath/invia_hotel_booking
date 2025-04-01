0dd65b036a0d1d4ffc8ba9c86ed5b15daf395d29
mpmnath <mpm.nath007@gmail.com>
2025-04-01 20:17:36 +0200

    Add constants for padding and icon sizes in values.dart

---
+++ lib/core/constants/values.dart
@@ -0,0 +1,22 @@
+import 'dart:ui';
+
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+// Padding constants
+double verySmallPadding = 4.r;
+double smallPadding = 8.r;
+double mediumPadding = 12.r;
+double normalPadding = 16.r;
+double bigPadding = 20.r;
+double veryBigPadding = 24.r;
+double headlinePadding = 12.r;
+double contentPadding = 24.r;
+const defaultDesignSize = Size(360, 780);
+const defaultTabletDesignSize = Size(768, 990);
+
+// Icon Size
+double verySmallIcon = 12.r;
+double smallIcon = 16.r;
+double mediumIcon = 18.r;
+double largeIcon = 24.r;
+double veryLargeIcon = 32.r;
